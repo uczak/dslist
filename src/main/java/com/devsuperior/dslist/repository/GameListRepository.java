12f9c420a58417fcc2a7340b996da63a0e95f5f5
Guilherme Uczak <guilherme.uczak@gruporbs.com.br>
2023-05-12 15:37:18 -0300

    add getLists

---
+++ src/main/java/com/devsuperior/dslist/repository/GameListRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.repository;
+
+import com.devsuperior.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+
+
+}
