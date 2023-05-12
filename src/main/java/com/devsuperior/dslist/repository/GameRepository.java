8c4da1757098029ef139a127965427c4760e26f8
Guilherme Uczak <guilherme.uczak@gruporbs.com.br>
2023-05-12 13:37:39 -0300

    adicionando primeira rota

---
+++ src/main/java/com/devsuperior/dslist/repository/GameRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.repository;
+
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
+
+}
